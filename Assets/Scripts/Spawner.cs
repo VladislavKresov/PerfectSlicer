f96f7067225c0f1543b8f0d83fe86278e21a7ffe
Vladislav Kresov <vladislavkresov@gmail.com>
2021-01-29 18:36:15 +0300

    Main mechanics

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _templates;
+    [SerializeField] private float _spawnDelay;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        while(true)
+        {
+            int foodIndex = Random.Range(0, _templates.Length);
+            Instantiate(_templates[foodIndex], transform.position, _templates[foodIndex].transform.rotation);
+            yield return new WaitForSeconds(_spawnDelay);
+        }
+    }
+}
